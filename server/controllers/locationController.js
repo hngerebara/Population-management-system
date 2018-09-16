d5671ad6ff3facc1a099c25e5ef653ffe7141cf5
Hope Ngerebara <hope.ngerebara@andela.com>
2018-09-16 23:44:03 +0100

    add api endpoints

---
+++ server/controllers/locationController.js
@@ -0,0 +1,95 @@
+const Location = require('../models/locationModel.js');
+
+
+module.exports = {
+    async createLocation(req, res) {
+      
+        try{
+            if(Object.keys(req.body).length === 0){
+                return res.status(400).json({message: 'Body cannot be empty'});
+            }
+
+            if(req.body.locations && req.body.locations.length){
+                req.body.totalPopulation = req.body.locations.reduce((acc, subPopulation) => {
+                    return acc + (subPopulation.femalePopulation + subPopulation.malePopulation);
+                  }, 0) + req.body.femalePopulation + req.body.malePopulation
+            } else {
+                req.body.totalPopulation = req.body.femalePopulation + req.body.malePopulation;
+            }
+
+            let location =  new Location(req.body);
+
+            let savedLocation = await location.save();
+            return res.status(201).json(savedLocation);
+
+        }catch(ex){
+            switch(ex.code){
+                case 11000:
+                    return res.status(404).json({message: "Location name already exists"});
+                default:
+                    return res.status(400).json(ex);
+           }   
+        } 
+    },
+
+    async allLocations(req, res) {
+        try{
+            let locations = await Location.find({});
+            if (!locations) return res.status(404).json({message: 'No location found'}); 
+        
+            return res.status(200).json(locations);
+        } catch(ex) {
+             return res.status(400).json(ex);
+        }
+    },
+
+    async retrieveLocation(req, res) {
+        try{
+            let location = await Location.findById(req.params.locationId);
+
+            if (!location) return res.status(404).json({message: 'Location Not Found', });
+            
+            return res.status(200).json(location);
+
+        }catch(ex){
+            return res.status(400).json(ex);
+        }
+    },
+
+    async updateLocation(req, res) {
+
+        try {
+            let location =  await Location.findById(req.params.locationId);
+
+            if (!location) return res.status(404).json({message: 'Location Not Found'});
+
+            location.name = req.body.name;
+            location.malePopulation = req.body.malePopulation;
+            location.femalePopulation = req.body.femalePopulation;
+
+            let updatedLocation = await location.save();
+            return res.status(200).json(updatedLocation);
+        
+        }catch(ex) {
+            switch(ex.code){
+                case 11000:
+                    return res.status(404).json({message: "Location name already exists"});
+                default:
+                    return res.status(400).json(ex);
+           }
+        };
+    },
+
+    async deleteLocation(req, res) {
+        try{
+            let location =  await Location.findById(req.params.locationId)
+            
+            if (!location) return res.status(404).json({message: 'Location Not Found', });
+            await location.remove()
+            return res.status(200).json({message: `Successfully deleted location with id ${req.params.locationId}`});
+        
+        }catch(ex){
+            return res.status(400).json(ex);
+        } 
+    },   
+};
